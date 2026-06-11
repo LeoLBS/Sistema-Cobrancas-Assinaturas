0d98cb76584abf7b43dbf3e8276aec14d75e6ad7
LeoLBS <leonardoperin70@gmail.com>
2026-06-10 22:47:35 -0300

    chore: inicializando a estrutura base do projeto com spring

---
+++ src/main/java/br/com/leperber/sistema_cobrancas_assinaturas/SistemaCobrancasAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package br.com.leperber.sistema_cobrancas_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCobrancasAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCobrancasAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leperber/sistema_cobrancas_assinaturas/SistemaCobrancasAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leperber.sistema_cobrancas_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCobrancasAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
