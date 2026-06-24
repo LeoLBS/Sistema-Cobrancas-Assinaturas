c7815812e08fbcc281af79230c1e461a5ab088dc
LeoLBS <leonardoperin70@gmail.com>
2026-06-23 23:04:11 -0300

    feat: implementando os metodos de leitura listarTodosMetodoPagamento e buscarPorId

--- src/main/java/br/com/leperber/sistema_cobrancas_assinaturas/service/MetodoPagamentoService.java
+++ src/main/java/br/com/leperber/sistema_cobrancas_assinaturas/service/MetodoPagamentoService.java
@@ -4,6 +4,8 @@ import br.com.leperber.sistema_cobrancas_assinaturas.model.MetodoPagamento;
 import br.com.leperber.sistema_cobrancas_assinaturas.repository.MetodoPagamentoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MetodoPagamentoService {
 
@@ -25,5 +27,13 @@ public class MetodoPagamentoService {
     private MetodoPagamento buscarMetodoPagamentoPorId(Long idMetodoPagamento){
         return metodoPagamentoRepository.findById(idMetodoPagamento)
                 .orElseThrow(() -> new IllegalArgumentException("Método de pagamento não encontrado!"));
+    }
+
+    public MetodoPagamento buscarPorId(Long idMetodoPagamento){
+        return buscarMetodoPagamentoPorId(idMetodoPagamento);
+    }
+
+    public List<MetodoPagamento> listarTodosMetodoPagamento(){
+        return metodoPagamentoRepository.findAll();
     }
 }
