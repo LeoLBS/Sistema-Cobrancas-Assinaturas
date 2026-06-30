72f9fb74a51cd04635ed6c6ab00c4ac71285da52
LeoLBS <leonardoperin70@gmail.com>
2026-06-29 23:55:51 -0300

    feat: criando a nova classe dto CancelaCobrancaDTO com os seus atributos

---
+++ src/main/java/br/com/leperber/sistema_cobrancas_assinaturas/dto/cobranca/CancelaCobrancaDTO.java
@@ -0,0 +1,15 @@
+package br.com.leperber.sistema_cobrancas_assinaturas.dto.cobranca;
+
+
+public class CancelaCobrancaDTO {
+
+    private Long idCobranca;
+
+    public Long getIdCobranca() {
+        return idCobranca;
+    }
+
+    public void setIdCobranca(Long idCobranca) {
+        this.idCobranca = idCobranca;
+    }
+}
